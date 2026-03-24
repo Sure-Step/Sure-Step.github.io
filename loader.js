64b237b3fdc8435fa8ce641aeb28d1e399cf1e03
kaibochan <kai.buckhalter@gmail.com>
2026-03-24 18:46:25 -0400

    Rebuild development repository to mimic previous project development websites

--- loader.js
+++
@@ -1,8 +0,0 @@
-// Retrieved from user mwiegboldt : https://stackoverflow.com/a/31837264
-$(function () {
-  var includes = $('[data-include]')
-  $.each(includes, function () {
-    var file = 'views/' + $(this).data('include') + '.html'
-    $(this).load(file)
-  })
-});
